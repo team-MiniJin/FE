220031c97aab0c51ef92f5b7ae470da2f64156ed
KimYouGyoung <tptkds12@gmail.com>
2024-05-27 23:45:51 +0900

    🚧 Creating my travel main page

--- src/app/my-travels/page.tsx
+++ src/app/my-travels/page.tsx
@@ -1,3 +1,17 @@
-export default function Home() {
-  return <div>home</div>;
+export default function MyTravels() {
+  return (
+    <div className="mt-10">
+      <div>
+        <div>
+          <h2 className="text-xl font-bold">다가오는 여행 일정</h2>
+        </div>
+        <div>캐러셀</div>
+      </div>
+      <div className="my-10 h-[1px] w-full border-t" />
+      <div>
+        <h2 className="text-xl font-bold">나의 여행 일정</h2>
+      </div>
+      <div>무한스크롤 리스트</div>
+    </div>
+  );
 }

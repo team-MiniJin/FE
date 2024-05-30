2817a5ee9dd8a777d6cbbbe06618493243941d97
KimYouGyoung <tptkds12@gmail.com>
2024-05-30 23:07:45 +0900

    💄UI: Add MyPost's like and scrap

--- src/widgets/creatingPlan/ui/CreatingPlan.tsx
+++ src/widgets/creatingPlan/ui/CreatingPlan.tsx
@@ -1,10 +1,11 @@
-// /* eslint-disable @typescript-eslint/no-unused-vars */
 // import { Form } from '@/components/ui/form';
 
-// export default function CreatingPlan() {
-//   return (
-//     <div>
-//       <h2 className="text-xl font-bold">새 여행 일정 만들기</h2>
-//     </div>
-//   );
-// }
+export default function CreatingPlan() {
+  return (
+    <div>
+      <div>
+        <h2 className="text-xl font-bold">새 여행 일정 만들기</h2>
+      </div>
+    </div>
+  );
+}
--- src/widgets/myPlans/ui/MyPlan.tsx
+++ src/widgets/myPlans/ui/MyPlan.tsx
@@ -2,11 +2,14 @@
 import { calculateStayDuration } from '@/shared';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { IoIosArrowRoundForward } from 'react-icons/io';
+import { IoBookmarkOutline } from 'react-icons/io5';
+
 import { MyPlanT } from '@/widgets/myPlans/types/myPlans';
+import { GoHeart } from 'react-icons/go';
 
 export default function MyPlan({ plan }: { plan: MyPlanT }) {
   return (
-    <li className="flex	cursor-pointer  items-center space-x-10 border-b px-3 py-8">
+    <li className="relative flex	cursor-pointer  items-center space-x-10 border-b px-3 py-8">
       <div className="h-[96px] w-[96px] rounded-full bg-gray-100" />
       <div className="space-y-2">
         <p className="inline-block rounded bg-[--brand-color] px-2 py-1 text-xs font-bold text-white">
@@ -36,6 +39,27 @@ export default function MyPlan({ plan }: { plan: MyPlanT }) {
               )}
             </div>
           ))}
+        </div>
+      </div>
+      <div className="absolute bottom-8 right-0 flex space-x-4 text-sm">
+        <p>{plan.scope ? '공개' : '비공개'}</p>
+        <div
+          className="flex items-center space-x-1"
+          aria-label={`좋아요 ${plan.number_of_likes}`}
+        >
+          <span>
+            <GoHeart className="text-base" />
+          </span>
+          <span>{plan.number_of_likes}</span>
+        </div>
+        <div
+          className="flex items-center space-x-1"
+          aria-label={`스크랩 ${plan.number_of_scraps}`}
+        >
+          <span>
+            <IoBookmarkOutline className="text-base" />
+          </span>
+          <span>{plan.number_of_scraps}</span>
         </div>
       </div>
     </li>

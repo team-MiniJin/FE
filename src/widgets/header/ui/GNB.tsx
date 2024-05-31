98db082b2a3830c4b4eb69e4175b55197ba59995
Finn <tptkds12@gmail.com>
2024-05-31 10:31:34 +0900

    🐛 Fix: Fix issue with active category display not working properly using usePathname Hook

--- src/widgets/header/ui/GNB.tsx
+++ src/widgets/header/ui/GNB.tsx
@@ -1,11 +1,18 @@
 'use client';
 
 import { GNB_CATEGORIES } from '@/widgets/header/consts/gnb';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { usePathname } from 'next/navigation';
 import GNBCategory from './GNBCategory';
 
 export default function GNB() {
+  const pathname = usePathname();
   const [activedCategory, setActivedCatogory] = useState<string>('');
+  useEffect(() => {
+    const curCategory = pathname.split('/')[1];
+    if (curCategory !== activedCategory) setActivedCatogory(curCategory);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [pathname]);
   return (
     <nav className="space-x-8">
       {Object.keys(GNB_CATEGORIES).map((categoryKey) => (

faf14f95d361b62a375b021c9d23ceabc5b87dd4
DESKTOP-S9RH0NN\I <jmjml15@naver.com>
2024-06-14 23:36:30 +0900

    💄 Layout: 회원가입,로그인,아이디비번찾기 페이지 스타일 수정
    shadcn, zod 라이브러리 사용

--- src/app/find-password/page.tsx
+++ src/app/find-password/page.tsx
@@ -1,5 +1,9 @@
 import { FindPasswordForm } from '@/widgets';
 
-export default function FindPassword() {
-  return <FindPasswordForm />;
+export default function NewFindPassword() {
+  return (
+    <div className="flex h-[calc(100vh-6rem)] flex-col items-center justify-center space-y-8">
+      <FindPasswordForm />
+    </div>
+  );
 }

a44c1e8fd6b0fc5b5a31e5b4afb3714baa53774b
DESKTOP-S9RH0NN\I <jmjml15@naver.com>
2024-06-14 22:51:49 +0900

    🔥 Remove: 여행정보 삭제
    여행정보 추후 merge예정

--- src/widgets/travel-info/components/Map.tsx
+++
@@ -1,7 +0,0 @@
-export default function Map() {
-  return (
-    <div className="flex h-[calc(100vh-5rem)] max-w-[calc(512px-2rem)] items-center justify-center bg-slate-300">
-      <div className="text-lg text-black">지도</div>
-    </div>
-  );
-}

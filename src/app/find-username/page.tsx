2e4b6a47b6e789b8bd57c75d3d19ca2331274e77
DESKTOP-S9RH0NN\I <jmjml15@naver.com>
2024-06-15 03:12:27 +0900

    🚚 Rename: 함수이름 오기 수정
    함수이름 수정

--- src/app/find-username/page.tsx
+++ src/app/find-username/page.tsx
@@ -1,6 +1,6 @@
 import { FindUsernameForm } from '@/widgets';
 
-export default function NewFindUsername() {
+export default function FindUsername() {
   return (
     <div className="flex h-[calc(100vh-6rem)] flex-col items-center justify-center space-y-8">
       <FindUsernameForm />

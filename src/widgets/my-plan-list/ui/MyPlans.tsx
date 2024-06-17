3703226a21877ed2dfaf05fd45b5392d3d96c16f
KimYouGyoung <tptkds12@gmail.com>
2024-06-17 17:14:46 +0900

    💄 layout: 나의 여행 일정 목록에 스켈레톤 UI 적용
    - query error, loading, fetching 상태에 따른 UI 개발

--- src/widgets/my-plan-list/ui/MyPlans.tsx
+++ src/widgets/my-plan-list/ui/MyPlans.tsx
@@ -1,13 +1,22 @@
 'use client';
 
+import { PiSpinnerGapLight } from 'react-icons/pi';
 import { useInfiniteScroll } from '@/shared';
 import useMyPlans from '@/widgets/my-plan-list/model/useMyPlans';
+import { Skeleton } from '@/components/ui/skeleton';
 import MyPlan from './MyPlan';
 import { MyPlanT } from '../types/myPlans';
 
 export default function MyPlans() {
-  const { data, fetchNextPage, hasNextPage, isFetchingNextPage, isFetching } =
-    useMyPlans();
+  const {
+    data,
+    fetchNextPage,
+    hasNextPage,
+    isFetchingNextPage,
+    isLoading,
+    isFetching,
+    isError,
+  } = useMyPlans();
   const observerRef = useInfiniteScroll(() => {
     if (hasNextPage && !isFetchingNextPage && !isFetching) fetchNextPage();
   }, hasNextPage);
@@ -17,18 +26,42 @@ export default function MyPlans() {
       <div>
         <h2 className="text-xl font-bold">나의 여행 일정</h2>
       </div>
-      <div>
-        <ul>
-          {data?.pages.map(
-            (page) =>
-              'data' in page &&
-              page.data.map((plan: MyPlanT) => (
-                <MyPlan key={plan.plan_id} plan={plan} />
-              ))
+      {isError && (
+        <div className="flex justify-center text-center">
+          <p className="mt-16">
+            나의 여행 일정 목록을 가져오는 중에 에러가 발생했어요.
+            <br /> 잠시 후 다시 시도해 주세요.
+          </p>
+        </div>
+      )}
+      {!isError && (
+        <div>
+          <ul>
+            {isLoading &&
+              new Array(10).fill(null).map((_, idx) => (
+                <div key={idx} className="relative my-4">
+                  <Skeleton className="absolute top-[50%] h-[50px] w-[50px] -translate-y-1/2 rounded-full bg-slate-100 md:h-[96px] md:w-[96px]"></Skeleton>
+                  <Skeleton className="ml-[70px] h-[170px] border-x-[6px] border-y-[22px] border-white py-6  md:ml-[132px] md:items-center md:px-3" />
+                </div>
+              ))}
+            {!isLoading &&
+              data &&
+              data.pages.map(
+                (page) =>
+                  'data' in page &&
+                  page.data.map((plan: MyPlanT) => (
+                    <MyPlan key={plan.plan_id} plan={plan} />
+                  ))
+              )}
+          </ul>
+          {isFetching && (
+            <div className="flex animate-spin justify-center	">
+              <PiSpinnerGapLight className="h-[32px] w-[32px]" />
+            </div>
           )}
-        </ul>
-        <div ref={observerRef} />
-      </div>
+          <div ref={observerRef} />
+        </div>
+      )}
     </div>
   );
 }
--- src/widgets/upcoming-plans/ui/UpcomingPlans.tsx
+++ src/widgets/upcoming-plans/ui/UpcomingPlans.tsx
@@ -8,6 +8,8 @@ import {
 import useUpcomingPlans from '@/widgets/upcoming-plans/model/useUpcomingPlans';
 import calculateDday from '@/shared/utils/calculateDday';
 import { Skeleton } from '@/components/ui/skeleton';
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
 import UpcomingPlan from './UpcomingPlan';
 
 const BREAK_POINTS: { [key: number]: number } = {
@@ -40,39 +42,51 @@ export default function UpcomingPlans() {
                   className="h-full w-full flex-shrink-0 border-x-[0.5rem] border-white md:w-1/2 lg:w-1/3"
                 />
               ))}
-          <button
-            type="button"
-            className="absolute left-0 top-2/4 z-10 -translate-y-2/4  text-4xl disabled:hidden"
-            aria-label="이전 슬라이드"
-            onClick={prevItem}
-            disabled={carouselStartIndex === 0}
-          >
-            <MdOutlineKeyboardArrowLeft />
-          </button>
-          {data?.map((plan) => {
-            if (calculateDday(plan.start_date) >= 0)
-              return (
-                <UpcomingPlan
-                  key={plan.plan_id}
-                  plan={plan}
-                  carouselStartIndex={carouselStartIndex}
-                  visibleSlides={visibleSlides}
-                />
-              );
-            return null;
-          })}
+          {!isLoading && data && (
+            <>
+              <button
+                type="button"
+                className="absolute left-0 top-2/4 z-10 -translate-y-2/4  text-4xl disabled:hidden"
+                aria-label="이전 슬라이드"
+                onClick={prevItem}
+                disabled={carouselStartIndex === 0}
+              >
+                <MdOutlineKeyboardArrowLeft />
+              </button>
+              {data.map((plan) => {
+                if (calculateDday(plan.start_date) >= 0)
+                  return (
+                    <UpcomingPlan
+                      key={plan.plan_id}
+                      plan={plan}
+                      carouselStartIndex={carouselStartIndex}
+                      visibleSlides={visibleSlides}
+                    />
+                  );
+                return null;
+              })}
+
+              <button
+                className="absolute right-0 top-1/2 -translate-y-1/2 text-4xl disabled:hidden"
+                type="button"
+                aria-label="다음 슬라이드"
+                onClick={nextItem}
+                disabled={
+                  carouselStartIndex >= (data?.length ?? 0) - visibleSlides
+                }
+              >
+                <MdOutlineKeyboardArrowRight />
+              </button>
+            </>
+          )}
+          {!isLoading && !data && (
+            <Button variant="outline" asChild>
+              <Link href="/create-plan">일정 만들러 가기</Link>
+            </Button>
+          )}
           {isLoading && (
             <Skeleton className="absolute right-0 top-1/2 h-[36px] w-[36px] -translate-y-1/2" />
           )}
-          <button
-            className="absolute right-0 top-1/2 -translate-y-1/2 text-4xl disabled:hidden"
-            type="button"
-            aria-label="다음 슬라이드"
-            onClick={nextItem}
-            disabled={carouselStartIndex >= (data?.length ?? 0) - visibleSlides}
-          >
-            <MdOutlineKeyboardArrowRight />
-          </button>
         </ul>
       </div>
     </div>

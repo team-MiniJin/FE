ef8f257e86d7dab506a6b8fba661d147ba6459bd
KimYouGyoung <tptkds12@gmail.com>
2024-06-19 18:33:19 +0900

    🔧 update: 서버 주소 https로 변경

--- src/shared/constants/api.ts
+++ src/shared/constants/api.ts
@@ -1,4 +1,4 @@
 export const MOCK_SERVER_URL =
   'https://ffa47e06-d63c-43a0-ac82-e53a67f0294e.mock.pstmn.io';
 export const KAKAO_LOCAL_URL = 'https://dapi.kakao.com/v2/local/search';
-export const TRAVEL_URL = 'http://lyckabc.synology.me:23080';
+export const TRAVEL_URL = 'https://lyckabc.synology.me:23080';

548458e66ddff4deb651d6085e32ce0289840621
KimYouGyoung <tptkds12@gmail.com>
2024-06-23 21:43:47 +0900

    💄 layout: Logo 추가
    - svg 대신 텍스트로 임시 적용

--- src/shared/ui/Logo.tsx
+++ src/shared/ui/Logo.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 function Logo() {
   return (
     <Link href="/" className="cursor-pointer text-2xl font-bold text-black">
-      Travel
+      <p className="select-none text-2xl font-bold">KORAVEL</p>
     </Link>
   );
 }

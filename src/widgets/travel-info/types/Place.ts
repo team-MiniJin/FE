eaf2c5d5fc88bed1f7fae3832d53bff0a0225cfc
DESKTOP-S9RH0NN\I <jmjml15@naver.com>
2024-06-25 03:57:03 +0900

    🔥 Remove: 미사용 타입 삭제
    미사용 타입 삭제

--- src/widgets/travel-info/types/Place.ts
+++ src/widgets/travel-info/types/Place.ts
@@ -1,25 +1,3 @@
-export interface PlaceT {
-  addr1: string;
-  addr2: string;
-  firstimage: string;
-  title: string;
-  contentid: number;
-}
-
-export interface PlaceDetailT {
-  homepage: string;
-  tel: string;
-  telname: string;
-  title: string;
-  firstimage: string;
-  addr1: string;
-  addr2: string;
-  mapx: number;
-  mapy: number;
-  mlevel: number;
-  overview: string;
-}
-
 export interface PlaceInfoT {
   addr1: string;
   addr2: string;

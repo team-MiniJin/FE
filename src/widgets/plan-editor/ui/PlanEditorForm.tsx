9ad688988c13134a353dd7227084d78df1c24529
KimYouGyoung <tptkds12@gmail.com>
2024-06-26 00:27:37 +0900

    🐛 fix: 일정 생성 및 수정 시 성공했다는 toast 메시지가 표시되지 않는 버그 해결

--- src/widgets/plan-editor/ui/PlanEditorForm.tsx
+++ src/widgets/plan-editor/ui/PlanEditorForm.tsx
@@ -98,6 +98,8 @@ export default function PlanEditorForm({
     isErrorUpdatePlan,
     isPendingCreatePlan,
     isPendingUpdatePlan,
+    isSuccessCreatePlan,
+    isSuccessUpdatePlan,
     toast,
   ]);
 
